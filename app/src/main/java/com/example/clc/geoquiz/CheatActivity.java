367c4f77109b8f3bcd2449118c6a9e83f0e6843e
yclvergil <yclVergil@gmail.com>
2016-03-10 00:30:11 +0800

    bug 待处理~~~

---
+++ app/src/main/java/com/example/clc/geoquiz/CheatActivity.java
@@ -0,0 +1,61 @@
+package com.example.clc.geoquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by clc on 2016/3/9.
+ */
+public class CheatActivity extends Activity {
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.example.clc.geoquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOWN = "com.example.clc.geoquiz.answer_shown";
+    private static final String CH_IS_CHEAT = "quiz_is_cheat";
+
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+    private boolean isCheat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        if (savedInstanceState != null) {
+            isCheat = savedInstanceState.getBoolean(CH_IS_CHEAT, false);
+        }
+        setAnswerShownResult(isCheat);
+        mAnswerTextView = (TextView) findViewById(R.id.showAnswerTextView);
+        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+                isCheat = true;
+            }
+        });
+
+    }
+
+    //设置传回的结果值
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(CH_IS_CHEAT, isCheat);
+    }
+}
--- app/src/main/java/com/example/clc/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/clc/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.example.clc.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +9,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String KEY_INDEX = "index";
+    private static final String SPIN_CHEAT = "quiz_is_cheat";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
+    private Button mCheatButton;
+    private boolean mIsCheater;
 
     private TrueFalse[] mQuestionBank = new TrueFalse[]{
             new TrueFalse(R.string.qu_a, true),
@@ -26,16 +32,25 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quiz);
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            if (savedInstanceState.getBoolean(SPIN_CHEAT, false)) {
+                mIsCheater = savedInstanceState.getBoolean(SPIN_CHEAT);
+            }
+        }
         updateQuestion();
         mNextButton = (Button) findViewById(R.id.next_button);
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
         //点击
         mTrueButton.setOnClickListener(this);
         mFalseButton.setOnClickListener(this);
         mNextButton.setOnClickListener(this);
+        mCheatButton.setOnClickListener(this);
     }
 
     @Override
@@ -50,12 +65,27 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.next_button:
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
+                break;
+            case R.id.cheat_button:
+                Intent i = new Intent(this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+                startActivityForResult(i, 0);
         }
+    }
+
+    //保存设备旋转前的数据
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+        outState.putBoolean(SPIN_CHEAT, mIsCheater);
     }
 
     //更新问题
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mIsCheater = false;
         mQuestionTextView.setText(question);
     }
 
@@ -63,11 +93,25 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
         int messageResId = 0;
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
         } else {
-            messageResId = R.string.incorrect_toast;
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
+
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    //onActivityResult()方法的实现
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
     }
 }
