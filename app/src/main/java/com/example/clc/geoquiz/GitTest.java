9af9b5a966a1bbffe558edb23f630d18e93c0213
yclvergil <yclVergil@gmail.com>
2016-03-09 11:01:53 +0800

    Test branch

---
+++ app/src/main/java/com/example/clc/geoquiz/GitTest.java
@@ -0,0 +1,10 @@
+package com.example.clc.geoquiz;
+
+/**
+ * Created by clc on 2016/3/9.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("This is a new branch");
+    }
+}
